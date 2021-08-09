217b8da86d4602094d9c4a20941ed9ebad00f740
Evgeny <evtsyga@gmail.com>
2021-08-09 13:58:04 +0300

    fix JoinBlock

--- client/chat_lite/src/components/JoinBlock.jsx
+++ client/chat_lite/src/components/JoinBlock.jsx
@@ -7,28 +7,27 @@ import React from 'react';
 function JoinBlock({ onLogin }) {
     const [roomId, setRoomID] = React.useState('')
     const [userName, setUserName] = React.useState('')
-    const [isLoading, setLoading] = React.useState(false)
 
     const onEnter = async () => {
         if (!roomId || !userName) {
-            return alert('Not Valid')
+            return alert('Please input all fields')
+        } else if (roomId.length > 10 || userName.length > 10) {
+            return alert('Max length 10 symbols')
+        } else {        
+            const obj = {
+                roomId,
+                userName
+            }
+            await axios.post('http://localhost:9999/rooms', obj)
+            onLogin(obj)
         }
-        const obj = {
-            roomId,
-            userName
-        }
-        setLoading(true)
-        await axios.post('http://localhost:9999/rooms', obj)
-        onLogin(obj)
     }
 
     return (
         <div>
             <input type='text' placeholder='Room ID' defaultValue={roomId} onChange={e => setRoomID(e.target.value)} />
-            <input type='text' placeholder='Your name' defaultValue={userName} onChange={e => setUserName(e.target.value)}/>
-            <button disabled={isLoading} onClick={() => onEnter()}>
-                {isLoading ? 'ENTRANCE' : 'TO ENTER'}
-            </button>
+            <input type='text' placeholder='Name max 10 symblos' defaultValue={userName} onChange={e => setUserName(e.target.value)}/>
+            <button onClick={() => onEnter()}>TO ENTER</button>
       </div>
     )
 }
